32fe6c5d388c16d00e493f8f01b1853402d50dd1
Devin Howard <devin@callysto.com>
2016-03-31 15:10:51 +0800

    publish v1.2.4

--- dist/redux-crud-store.js
+++ dist/redux-crud-store.js
@@ -9505,6 +9505,8 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
+	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 	exports.default = crudReducer;
 
 	var _immutable = __webpack_require__(116);
@@ -9644,12 +9646,18 @@ return /******/ (function(modules) { // webpackBootstrap
 	        return state.push(collectionReducer(undefined, action));
 	      }
 	      // update the entry with the same params as before
+
+	      var _entry = _slicedToArray(entry, 2);
+
+	      var index = _entry[0];
+	      var existingCollection = _entry[1];
+
 	      var alteredAction = _extends({}, action, {
 	        meta: _extends({}, action.meta, {
-	          params: entry.params
+	          params: action.meta.params || existingCollection.params
 	        })
 	      });
-	      return state.update(entry[0], function (s) {
+	      return state.update(index, function (s) {
 	        return collectionReducer(s, alteredAction);
 	      });
 	    default:
