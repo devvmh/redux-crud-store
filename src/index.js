f00035dc41380a5ae5914b732c5caf3ccc1e52b2
Devin Howard <devin@callysto.com>
2016-05-25 19:08:44 +0800

    implement default API client. Once it is tested, fixes #2

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@
 import crudSaga from './sagas'
 import crudReducer from './reducers'
 import * as crudActions from './actionTypes'
+import ApiClient from './ApiClient'
 
-export { crudSaga, crudReducer, crudActions }
+export { crudSaga, crudReducer, crudActions, ApiClient }
 
 export {
   fetchCollection, fetchRecord, createRecord, updateRecord, deleteRecord,
