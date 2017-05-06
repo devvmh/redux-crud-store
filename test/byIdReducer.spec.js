bb2f5a53cebdaef035ffab2494c5301c70188924
Devin Howard <devin@callysto.com>
2017-05-06 08:52:25 -0700

    byIdReducer delete actions tests

--- test/byIdReducer.spec.js
+++ test/byIdReducer.spec.js
@@ -23,17 +23,42 @@ const sampleError = fromJS({
   error: {}
 })
 
-describe('GARBAGE_COLLECT', () => {
-  const initialState = fromJS({
-    0: sampleRecord.set('fetchTime', Date.now() - 11 * 60 * 1000), // more than ten minutes ago
-    1: sampleRecord
+describe('byIdReducer', () => {
+  describe('DELETE actions', () => {
+    const initialState = fromJS({
+      0: sampleRecord,
+      1: sampleRecord
+    })
+
+    it('does nothing for DELETE', () => {
+      const action = { type: DELETE, meta: { id: 1 } }
+      expect(byIdReducer(initialState, action)).toEqual(initialState)
+    })
+    it('does nothing for DELETE_ERROR', () => {
+      const action = { type: DELETE_ERROR, meta: { id: 1 } }
+      expect(byIdReducer(initialState, action)).toEqual(initialState)
+    })
+    it('removes record on DELETE_SUCCESS', () => {
+      const action = { type: DELETE_SUCCESS, meta: { id: 1 } }
+      const newState = byIdReducer(initialState, action)
+      expect(newState.get('0')).toEqual(sampleRecord)
+      expect(newState.get('1')).toEqual(undefined)
+      expect(Object.keys(newState.toJS()).length).toEqual(1)
+    })
   })
 
-  it('clears records older than 10 minutes', () => {
-    const action = { type: GARBAGE_COLLECT, meta: { now: Date.now() } }
-    const newState = byIdReducer(initialState, action)
-    expect(Object.keys(newState.toJS()).length).toEqual(1)
-    expect(newState.get('0')).toEqual(undefined)
-    expect(newState.get('1')).toEqual(sampleRecord)
+  describe('GARBAGE_COLLECT', () => {
+    const initialState = fromJS({
+      0: sampleRecord.set('fetchTime', Date.now() - 11 * 60 * 1000), // more than ten minutes ago
+      1: sampleRecord
+    })
+
+    it('clears records older than 10 minutes', () => {
+      const action = { type: GARBAGE_COLLECT, meta: { now: Date.now() } }
+      const newState = byIdReducer(initialState, action)
+      expect(Object.keys(newState.toJS()).length).toEqual(1)
+      expect(newState.get('0')).toEqual(undefined)
+      expect(newState.get('1')).toEqual(sampleRecord)
+    })
   })
 })
