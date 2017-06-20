6a0d00d78a631bfb9831880b1d3311c2e28c1629
Devin Howard <devin@callysto.com>
2017-06-19 20:42:32 -0700

    fix redux-saga deprecation warnings

--- src/sagas.js
+++ src/sagas.js
@@ -1,8 +1,7 @@
 /* @flow */
 /* global Generator */
 
-import { takeEvery } from 'redux-saga'
-import { fork, put, call } from 'redux-saga/effects'
+import { all, call, fork, put, takeEvery } from 'redux-saga/effects'
 
 import {
   FETCH, FETCH_ONE, CREATE, UPDATE, DELETE, API_CALL, GARBAGE_COLLECT
@@ -47,32 +46,32 @@ function* _apiGeneric(action: CrudAction<any>): Generator<Effect, void, any> {
 }
 
 const watchFetch = (apiClient) => function* _watchFetch() {
-  yield* takeEvery(FETCH, apiGeneric(apiClient))
+  yield takeEvery(FETCH, apiGeneric(apiClient))
 }
 
 const watchFetchOne = (apiClient) => function* _watchFetchOne() {
-  yield* takeEvery(FETCH_ONE, apiGeneric(apiClient))
+  yield takeEvery(FETCH_ONE, apiGeneric(apiClient))
 }
 
 const watchCreate = (apiClient) => function* _watchCreate() {
-  yield* takeEvery(CREATE, apiGeneric(apiClient))
+  yield takeEvery(CREATE, apiGeneric(apiClient))
 }
 
 const watchUpdate = (apiClient) => function* _watchUpdate() {
-  yield* takeEvery(UPDATE, apiGeneric(apiClient))
+  yield takeEvery(UPDATE, apiGeneric(apiClient))
 }
 
 const watchDelete = (apiClient) => function* _watchDelete() {
-  yield* takeEvery(DELETE, apiGeneric(apiClient))
+  yield takeEvery(DELETE, apiGeneric(apiClient))
 }
 
 const watchApiCall = (apiClient) => function* _watchApiCall() {
-  yield* takeEvery(API_CALL, apiGeneric(apiClient))
+  yield takeEvery(API_CALL, apiGeneric(apiClient))
 }
 
 export default function crudSaga(apiClient: Object) {
   return function* _crudSaga(): Generator<Effect, void, any> {
-    yield [
+    yield all([
       fork(watchFetch(apiClient)),
       fork(watchFetchOne(apiClient)),
       fork(watchCreate(apiClient)),
@@ -80,6 +79,6 @@ export default function crudSaga(apiClient: Object) {
       fork(watchDelete(apiClient)),
       fork(watchApiCall(apiClient)),
       fork(garbageCollector)
-    ]
+    ])
   }
 }
