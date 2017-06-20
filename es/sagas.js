3f97732173ce0d8bc047dca0f41d2b69ac801826
Devin Howard <devin@callysto.com>
2017-06-19 21:29:47 -0700

    publish v5.2.3

--- es/sagas.js
+++ es/sagas.js
@@ -5,43 +5,33 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.apiGeneric = undefined;
 
-var _extends2 = require('babel-runtime/helpers/extends');
-
-var _extends3 = _interopRequireDefault(_extends2);
-
-var _regenerator = require('babel-runtime/regenerator');
-
-var _regenerator2 = _interopRequireDefault(_regenerator);
-
-var _promise = require('babel-runtime/core-js/promise');
-
-var _promise2 = _interopRequireDefault(_promise);
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+/* global Generator */
 
 exports.default = crudSaga;
-
-require('regenerator-runtime/runtime');
 
 var _effects = require('redux-saga/effects');
 
 var _actionTypes = require('./actionTypes');
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var _marked = [garbageCollector].map(_regenerator2.default.mark);
-/* global Generator */
-
 // Generator type parameters are: Generator<+Yield,+Return,-Next>
+
+// NOTE: need to avoid hoisting generator functions or they'll happen
+// before this definition. See garbageCollector definition below, e.g.
+
 
 // TODO: The `Effect` type is not actually defined. Because 'redux-saga' does
 // not use  annotations, flow pretends that this import succeeds.
+var regeneratorRuntime = require('regenerator-runtime');
+
 var delay = function delay(ms) {
-  return new _promise2.default(function (resolve) {
+  return new Promise(function (resolve) {
     return setTimeout(resolve, ms);
   });
 };
 
-function garbageCollector() {
-  return _regenerator2.default.wrap(function garbageCollector$(_context) {
+var garbageCollector = regeneratorRuntime.mark(function garbageCollector() {
+  return regeneratorRuntime.wrap(function garbageCollector$(_context) {
     while (1) {
       switch (_context.prev = _context.next) {
         case 0:
@@ -65,20 +55,20 @@ function garbageCollector() {
           return _context.stop();
       }
     }
-  }, _marked[0], this);
-}
+  }, garbageCollector, this);
+});
 
 var apiGeneric = exports.apiGeneric = function apiGeneric(apiClient) {
-  return _regenerator2.default.mark(function _apiGeneric(action) {
+  return regeneratorRuntime.mark(function _apiGeneric(action) {
     var _action$payload, method, path, params, data, fetchConfig, _action$meta, success, failure, meta, response;
 
-    return _regenerator2.default.wrap(function _apiGeneric$(_context2) {
+    return regeneratorRuntime.wrap(function _apiGeneric$(_context2) {
       while (1) {
         switch (_context2.prev = _context2.next) {
           case 0:
             _action$payload = action.payload, method = _action$payload.method, path = _action$payload.path, params = _action$payload.params, data = _action$payload.data, fetchConfig = _action$payload.fetchConfig;
             _action$meta = action.meta, success = _action$meta.success, failure = _action$meta.failure;
-            meta = (0, _extends3.default)({}, action.meta, {
+            meta = _extends({}, action.meta, {
               fetchTime: Date.now()
             });
             _context2.prev = 3;
@@ -110,14 +100,15 @@ var apiGeneric = exports.apiGeneric = function apiGeneric(apiClient) {
 };
 
 var watchFetch = function watchFetch(apiClient) {
-  return _regenerator2.default.mark(function _watchFetch() {
-    return _regenerator2.default.wrap(function _watchFetch$(_context3) {
+  return regeneratorRuntime.mark(function _watchFetch() {
+    return regeneratorRuntime.wrap(function _watchFetch$(_context3) {
       while (1) {
         switch (_context3.prev = _context3.next) {
           case 0:
-            return _context3.delegateYield((0, _effects.takeEvery)(_actionTypes.FETCH, apiGeneric(apiClient)), 't0', 1);
+            _context3.next = 2;
+            return (0, _effects.takeEvery)(_actionTypes.FETCH, apiGeneric(apiClient));
 
-          case 1:
+          case 2:
           case 'end':
             return _context3.stop();
         }
@@ -127,14 +118,15 @@ var watchFetch = function watchFetch(apiClient) {
 };
 
 var watchFetchOne = function watchFetchOne(apiClient) {
-  return _regenerator2.default.mark(function _watchFetchOne() {
-    return _regenerator2.default.wrap(function _watchFetchOne$(_context4) {
+  return regeneratorRuntime.mark(function _watchFetchOne() {
+    return regeneratorRuntime.wrap(function _watchFetchOne$(_context4) {
       while (1) {
         switch (_context4.prev = _context4.next) {
           case 0:
-            return _context4.delegateYield((0, _effects.takeEvery)(_actionTypes.FETCH_ONE, apiGeneric(apiClient)), 't0', 1);
+            _context4.next = 2;
+            return (0, _effects.takeEvery)(_actionTypes.FETCH_ONE, apiGeneric(apiClient));
 
-          case 1:
+          case 2:
           case 'end':
             return _context4.stop();
         }
@@ -144,14 +136,15 @@ var watchFetchOne = function watchFetchOne(apiClient) {
 };
 
 var watchCreate = function watchCreate(apiClient) {
-  return _regenerator2.default.mark(function _watchCreate() {
-    return _regenerator2.default.wrap(function _watchCreate$(_context5) {
+  return regeneratorRuntime.mark(function _watchCreate() {
+    return regeneratorRuntime.wrap(function _watchCreate$(_context5) {
       while (1) {
         switch (_context5.prev = _context5.next) {
           case 0:
-            return _context5.delegateYield((0, _effects.takeEvery)(_actionTypes.CREATE, apiGeneric(apiClient)), 't0', 1);
+            _context5.next = 2;
+            return (0, _effects.takeEvery)(_actionTypes.CREATE, apiGeneric(apiClient));
 
-          case 1:
+          case 2:
           case 'end':
             return _context5.stop();
         }
@@ -161,14 +154,15 @@ var watchCreate = function watchCreate(apiClient) {
 };
 
 var watchUpdate = function watchUpdate(apiClient) {
-  return _regenerator2.default.mark(function _watchUpdate() {
-    return _regenerator2.default.wrap(function _watchUpdate$(_context6) {
+  return regeneratorRuntime.mark(function _watchUpdate() {
+    return regeneratorRuntime.wrap(function _watchUpdate$(_context6) {
       while (1) {
         switch (_context6.prev = _context6.next) {
           case 0:
-            return _context6.delegateYield((0, _effects.takeEvery)(_actionTypes.UPDATE, apiGeneric(apiClient)), 't0', 1);
+            _context6.next = 2;
+            return (0, _effects.takeEvery)(_actionTypes.UPDATE, apiGeneric(apiClient));
 
-          case 1:
+          case 2:
           case 'end':
             return _context6.stop();
         }
@@ -178,14 +172,15 @@ var watchUpdate = function watchUpdate(apiClient) {
 };
 
 var watchDelete = function watchDelete(apiClient) {
-  return _regenerator2.default.mark(function _watchDelete() {
-    return _regenerator2.default.wrap(function _watchDelete$(_context7) {
+  return regeneratorRuntime.mark(function _watchDelete() {
+    return regeneratorRuntime.wrap(function _watchDelete$(_context7) {
       while (1) {
         switch (_context7.prev = _context7.next) {
           case 0:
-            return _context7.delegateYield((0, _effects.takeEvery)(_actionTypes.DELETE, apiGeneric(apiClient)), 't0', 1);
+            _context7.next = 2;
+            return (0, _effects.takeEvery)(_actionTypes.DELETE, apiGeneric(apiClient));
 
-          case 1:
+          case 2:
           case 'end':
             return _context7.stop();
         }
@@ -195,14 +190,15 @@ var watchDelete = function watchDelete(apiClient) {
 };
 
 var watchApiCall = function watchApiCall(apiClient) {
-  return _regenerator2.default.mark(function _watchApiCall() {
-    return _regenerator2.default.wrap(function _watchApiCall$(_context8) {
+  return regeneratorRuntime.mark(function _watchApiCall() {
+    return regeneratorRuntime.wrap(function _watchApiCall$(_context8) {
       while (1) {
         switch (_context8.prev = _context8.next) {
           case 0:
-            return _context8.delegateYield((0, _effects.takeEvery)(_actionTypes.API_CALL, apiGeneric(apiClient)), 't0', 1);
+            _context8.next = 2;
+            return (0, _effects.takeEvery)(_actionTypes.API_CALL, apiGeneric(apiClient));
 
-          case 1:
+          case 2:
           case 'end':
             return _context8.stop();
         }
@@ -212,13 +208,13 @@ var watchApiCall = function watchApiCall(apiClient) {
 };
 
 function crudSaga(apiClient) {
-  return _regenerator2.default.mark(function _crudSaga() {
-    return _regenerator2.default.wrap(function _crudSaga$(_context9) {
+  return regeneratorRuntime.mark(function _crudSaga() {
+    return regeneratorRuntime.wrap(function _crudSaga$(_context9) {
       while (1) {
         switch (_context9.prev = _context9.next) {
           case 0:
             _context9.next = 2;
-            return [(0, _effects.fork)(watchFetch(apiClient)), (0, _effects.fork)(watchFetchOne(apiClient)), (0, _effects.fork)(watchCreate(apiClient)), (0, _effects.fork)(watchUpdate(apiClient)), (0, _effects.fork)(watchDelete(apiClient)), (0, _effects.fork)(watchApiCall(apiClient)), (0, _effects.fork)(garbageCollector)];
+            return (0, _effects.all)([(0, _effects.fork)(watchFetch(apiClient)), (0, _effects.fork)(watchFetchOne(apiClient)), (0, _effects.fork)(watchCreate(apiClient)), (0, _effects.fork)(watchUpdate(apiClient)), (0, _effects.fork)(watchDelete(apiClient)), (0, _effects.fork)(watchApiCall(apiClient)), (0, _effects.fork)(garbageCollector)]);
 
           case 2:
           case 'end':
