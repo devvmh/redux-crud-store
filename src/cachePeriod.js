c681836f0afd5e64aed9429bf37542b564cc97e0
Devin Howard <devin@callysto.com>
2017-09-16 13:54:46 -0700

    make cachePeriod configurable via environment variables

--- src/cachePeriod.js
+++ src/cachePeriod.js
@@ -1,21 +1,48 @@
 const TEN_MINUTES = 10 * 60 * 1000
 
 // private
-function getCachePeriod() {
-  return TEN_MINUTES
+function getCachePeriod({ showMessages = true }) {
+  let value = process.env.CACHE_PERIOD
+    || process.env.REACT_APP_CACHE_PERIOD
+    || TEN_MINUTES
+  value = Math.round(value)
+  if (isNaN(value)) {
+    value = TEN_MINUTES
+    if (showMessages) {
+      devMessage("getCachePeriod got NaN! Falling back to ten minute cache period")
+    }
+  }
+  if (value < 1000) {
+    value = TEN_MINUTES
+    if (showMessages) {
+      devMessage("getCachePeriod got a value under one second! Falling back to ten minute cache period")
+    }
+  }
+  return value
 }
 
 // private
 function getHalfCachePeriod() {
-  return TEN_MINUTES / 2
-}
-
-function cachePeriodAgo(now) {
-  const cachePeriod = TEN_MINUTES
-  return now - cachePeriod
+  let value = process.env.HALF_CACHE_PERIOD
+    || process.env.REACT_APP_HALF_CACHE_PERIOD
+    || getCachePeriod() / 2
+  value = Math.round(value)
+  if (isNaN(value)) {
+    value = Math.round(getCachePeriod({ showMessages: false }) / 2)
+    devMessage(`getHalfCachePeriod got NaN! Falling back to half of cachePeriod: ${value}`)
+  }
+  if (value < 500) {
+    value = Math.round(getCachePeriod({ showMessages: false }) / 2)
+    devMessage(`getHalfCachePeriod got a value under 500ms! Falling back to half of cache period: ${value}`)
+  }
+  return value
 }
 
 const cachePeriod = getCachePeriod()
 const halfCachePeriod = getHalfCachePeriod()
+
+function cachePeriodAgo(now) {
+  return now - cachePeriod
+}
 
 export { cachePeriodAgo, cachePeriod, halfCachePeriod }
